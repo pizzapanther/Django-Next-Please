d45ed288612e4a951a5ba32b9e000836077068f5
Paul Bailey <paul.m.bailey@gmail.com>
2012-06-06 23:17:48 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Django-Next-Please",
+    version = '12.06.1',
+    description = "Simple Pagination Decorator for Django.",
+    url = "https://github.com/pizzapanther/Django-Next-Please",
+    author = "Paul Bailey",
+    author_email = "paul.m.bailey@gmail.com",
+    license = "BSD",
+    packages = ['NextPlease'],
+    include_package_data = True,
+)
