377cbe21dbb1bd598e2ec799d0a51dc63697e485
Paul Bailey <paul.m.bailey@gmail.com>
2013-12-10 00:47:57 +0000

    updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Django-Next-Please",
-    version = '13.03',
+    version = '13.12',
     description = "Simple Pagination Decorator for Django.",
     url = "https://github.com/pizzapanther/Django-Next-Please",
     author = "Paul Bailey",
